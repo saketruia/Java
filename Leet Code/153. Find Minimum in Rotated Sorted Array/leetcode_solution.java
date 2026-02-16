407f85ea9b8c171778540ab019ef51dc1454da8a
Saket <saketruia18@gmail.com>
2026-02-16 16:57:23 +0530

    153 of leetcode

---
+++ Leet Code/153. Find Minimum in Rotated Sorted Array/leetcode_solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int left=0;
+        int right=nums.length-1;
+        int mid=right+1 /2;
+        while(left<right){
+            if(nums[mid]>nums[right]){
+                left=mid+1;
+            }else{
+                right=mid;
+            }
+            mid=left + (right-left)/2;
+        }
+        return nums[left];
+    }
+}
